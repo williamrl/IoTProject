6be24d36ac4d50770e99e347e86285d7679d1c91
dzw5600 <dzw5600@psu.edu>
2025-02-26 12:32:25 -0500

    Initial Server and DB Setup

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'SmartHome'
+app.config['MYSQL_PASSWORD'] = 'SmartHomePassword'
+app.config['MYSQL_DB'] = 'SmartHomeMonitoringSystem'
+ 
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE email = % s AND password = % s', (email, password, ))
+        account = cursor.fetchone()
+
+        if account:
+            return redirect('/home')
+        else:
+            return redirect('/')
+        
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+
+        if account:
+            return redirect('/register')
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s)',(email, password,))
+            mysql.connection.commit()
+            return redirect('/home')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+app.run(debug=True)
