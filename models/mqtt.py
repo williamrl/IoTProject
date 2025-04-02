bdded5ea60c98fe1ccf730bbda3d9059df22b9e5
unknown <dzw5600@psu.edu>
2025-04-02 12:33:08 -0400

    'fix'

--- models/mqtt.py
+++ models/mqtt.py
@@ -3,6 +3,6 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-def handle_publish(topic, message):
+def publish_handler(topic, message):
     channel.queue_declare(queue=topic)
     channel.basic_publish(exchange='', routing_key=topic, body=message)
